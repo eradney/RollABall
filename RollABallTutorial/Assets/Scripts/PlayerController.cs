1e90b5d94ef88af495ecc18babc756e795892436
eradney <eradney@knights.ucf.edu>
2019-07-08 18:21:42 -0400

    Tier 2 Commit

---
+++ RollABallTutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text scoreText;
+    public Text loseText;
+
+    private Rigidbody rb;
+    private int count;
+    private int lives;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        lives = 3;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+            SetScoreText();
+        }
+        if (count == 12)
+        {
+             transform.position = new Vector3(22.0f, transform.position.y,8.0f); 
+        }
+        if (lives == 0)
+        {
+            Destroy(this);
+            void DestroyComponent()
+            {
+                Destroy(GetComponent<Rigidbody>());
+            }
+            loseText.text = "You Lose :(";
+        }
+    
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 20)
+        {
+            winText.text = "You Win!";
+
+        }
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = "Lives: " + lives.ToString();
+    }
+}
